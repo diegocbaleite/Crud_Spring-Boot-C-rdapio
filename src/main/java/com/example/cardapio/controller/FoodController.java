ee10dbb2fc4c3d35434baf433acd67d15eb9d8cd
Diego Leite <diego.leite@sefaz.mt.gov.br>
2025-10-02 13:14:54 -0400

    Initial commit for Cardapio Spring Boot project

---
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -0,0 +1,35 @@
+package com.example.cardapio.controller;
+
+import com.example.cardapio.dto.FoodRequestDTO;
+import com.example.cardapio.dto.FoodResponseDTO;
+import com.example.cardapio.model.Food;
+import com.example.cardapio.repository.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/food")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public ResponseEntity<String> saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return ResponseEntity.ok("Food cadastrado com sucesso!");
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll() {
+
+        return repository.findAll().stream().map(FoodResponseDTO::new).toList();
+    }
+}
